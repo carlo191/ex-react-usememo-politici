5be0937bc070a85efc58b1c78581fe7cfaf731bb
Carlo Elvisi <carloelvisi01@outlook.it>
2025-03-19 19:35:47 +0100

    milestone 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,39 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 
 import "./index.css";
 
 function App() {
   const [count, setCount] = useState(0);
   const [politici, setPolitici] = useState([]);
+  const [search, setSearch] = useState("");
   useEffect(() => {
     fetch("https://boolean-spec-frontend.vercel.app/freetestapi/politicians")
       .then((res) => res.json())
       .then((data) => setPolitici(data))
       .catch((error) => console.error(error));
   }, []);
-
+  const filteredPolitici = useMemo(() => {
+    return politici.filter((p) => {
+      const isNameIncluded = p.name
+        .toLowerCase()
+        .includes(search.toLowerCase());
+      const isBioIncluded = p.biography
+        .toLowerCase()
+        .includes(search.toLowerCase());
+      return isNameIncluded || isBioIncluded;
+    });
+  }, [politici, search]);
   return (
     <>
+      <input
+        type="text"
+        placeholder="Cerca"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
       <h1>Lista Politici</h1>
       <div className="politici-list">
-        {politici.map((p) => (
+        {filteredPolitici.map((p) => (
           <div className="card" key={p.id}>
             <img src={p.image} alt={p.name} />
             <h2>{p.name}</h2>
